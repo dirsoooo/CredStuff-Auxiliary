5f7fed47e8e747eed68c2c1d144d4d0858834c3f
Dirso <73609472+dirsoooo@users.noreply.github.com>
2021-05-25 00:04:19 -0300

    Update main.sh

--- CredStuff_Auxiliary/main.sh
+++ CredStuff_Auxiliary/main.sh
@@ -1,11 +1,11 @@
-#!/bin/sh 
-googler_call="python3 /home/pdz/Desktop/CredStuff_Auxiliary/googler/googler" #aqui vocẽ deve colocar onde está a tool googler em seu ambiente
+#!/usr/bin/env bash
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+googler_call="python3 $SCRIPTPATH/googler/googler --noprompt"
 anonfiles_search1="site:throwbin.io OR site:pastebin.com OR site:cdn-*anonfiles.com AND intext:@" 
 anonfiles_search2=$anonfiles_search1$1
 anonfiles_unfilter="--unfilter"
 googler_count="--count $2"
-echo $googler_call $anonfiles_search2 $googler_count $anonfiles_unfilter > ./temp/dork_test1.sh
-chmod +x ./temp/dork_test1.sh
-sh ./temp/dork_test1.sh | grep $1
-
-	
+echo $googler_call $anonfiles_search2 $googler_count $anonfiles_unfilter > $SCRIPTPATH/temp/dork_test1.sh
+chmod +x $SCRIPTPATH/temp/dork_test1.sh
+sh $SCRIPTPATH/temp/dork_test1.sh
