7ea1b5258d8381a3e044ec7f860bd8e1431172ee
Dirso <73609472+dirsoooo@users.noreply.github.com>
2021-05-25 00:08:53 -0300

    Update dork_test1.sh

--- CredStuff_Auxiliary/temp/dork_test1.sh
+++ CredStuff_Auxiliary/temp/dork_test1.sh
@@ -1,1 +1,2 @@
-python3 /home/pdz/Desktop/CredStuff_Auxiliary/googler/googler site:throwbin.io OR site:pastebin.com OR site:cdn-*anonfiles.com AND intext:@bradesco.com --count 1000 --unfilter
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+python3 $SCRIPTPATH/tools/CredStuff-Auxiliary/CredStuff_Auxiliary/googler/googler site:throwbin.io OR site:pastebin.com OR site:cdn-*anonfiles.com AND intext:@target.com --count 100 --unfilter
